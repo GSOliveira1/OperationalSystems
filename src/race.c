651864f63e025ec4fd79e74bde4121179405cfc1
Gabriel Souza <gabrielsouza010208@gmail.com>
2024-11-25 14:32:10 -0300

    Adding pthread condition and making last adjustments.

--- src/race.c
+++ src/race.c
@@ -6,11 +6,13 @@
 #include <time.h>
 
 pthread_mutex_t lock;
+pthread_cond_t condition = PTHREAD_COND_INITIALIZER;
 int winner = -1;
+int game_over = 0;
 time_t start_time, end_time;
 
 int game_simulation(){
-    pthread_t players[NUM_PLAYERS]; /* Threads array */
+    pthread_t players[NUM_PLAYERS]; /* Array de Threads */
     int ids_players[NUM_PLAYERS];
 
     if (pthread_mutex_init(&lock, NULL)){
@@ -37,6 +39,7 @@ int game_simulation(){
     }
 
     pthread_mutex_destroy(&lock);
+    pthread_cond_destroy(&condition);
 
     return 0;
 }
@@ -44,15 +47,22 @@ int game_simulation(){
 void *player(void *arg){
     int score = 0;
     int id = *(int *)arg;
+    // Loop responsável pela corrida:
+    while (1){ 
+        pthread_mutex_lock(&lock);
 
-    while (score < POINTS_TO_WIN){
-        if (winner != -1)
+        if (game_over){
+            pthread_mutex_unlock(&lock);
             break;
+        }
 
-        usleep(50000);
+        pthread_mutex_unlock(&lock);
+
+        usleep(50000);  // Atraso fixo
         usleep(rand() % 5000);  // Simula o tempo para marcar os pontos;
         int points_scored =  (rand() % 3) + 1;  // Marca entre 1 e 3 pontos;
         score += points_scored;
+        
         printf("Player %d scored %d points in total!\n", id, score);
 
         if (score > POINTS_TO_WIN)
@@ -61,13 +71,15 @@ void *player(void *arg){
         if (score >= POINTS_TO_WIN){
             pthread_mutex_lock(&lock);
 
-            // Verifica e define o vencedor se ainda não houver um
+            // Verifica e define o vencedor se ainda não houver um:
             if (winner == -1){
                 winner = id;
                 time(&end_time);
                 printf("\nPlayer number %d is the winner!\n", id);
                 double time_taken = difftime(end_time, start_time);
                 printf("Player %d took: %.2f seconds to win!\n", id, time_taken);
+                game_over = 1;
+                pthread_cond_broadcast(&condition);
             }
 
             pthread_mutex_unlock(&lock);
